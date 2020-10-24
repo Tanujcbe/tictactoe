1f29d9742b5ffede977b6c3e2446a34cb30c68a5
yavuz.demir <yavuz.demir@trendyol.com>
2020-10-24 19:18:27 +0300

    Code Refactor

--- tictactoe.py
+++ tictactoe.py
@@ -1,199 +1,135 @@
-class Board:
-    def __init__(self, row, col, target):
-        self.row = row
-        self.col = col
-        self.target = target
-        self.board = []
-        self.initialize_board()
-        self.number_of_remaining_moves = row * col
+class PlayerEnv:
+    def __init__(self, bound):
+        self.human = Human('X', bound)
+        self.computer = Computer('O', bound)
+        self.human_turn = True
 
-    def initialize_board(self):
-        for r in range(self.row):
-            self.board.append([])
-            for c in range(self.col):
-                self.board[r].append(' ')
+    def move(self, board_obj):
+        if self.human_turn:
+            self.human.make_move(board_obj)
+        else:
+            self.computer.make_move(board_obj)
 
-    def is_board_full(self):
-        return self.number_of_remaining_moves == 0
+    def change_turn(self):
+        self.human_turn = not self.human_turn
 
-    def print_table(self):
-        print('-' * (4 * (self.col - 1) + 3))
-        for r in range(self.row):
-            print(' ', end="")
-            for c in range(self.col):
-                print(str(self.board[r][c]) + ' ', end="")
-                if c == self.col - 1:
-                    print(' ')
-                else:
-                    print('| ', end="")
-            print('-' * (4 * (self.col - 1) + 3))
+    def get_last_row(self):
+        if self.human_turn:
+            return self.human.get_last_row()
+        else:
+            return self.computer.get_last_row()
 
-    def is_winning(self, r, c, marker):
-        return self.check_column(r, c, marker) or self.check_row(r, c, marker) \
-               or self.check_main_diagonal(r, c, marker) or self.check_secondary_diagonal(r, c, marker)
+    def get_last_col(self):
+        if self.human_turn:
+            return self.human.get_last_col()
+        else:
+            return self.computer.get_last_col()
 
-    def check_column(self, r, c, marker):
-        number_of_marker = 0
-
-        cur_row = r
-        while cur_row >= 0 and self.board[cur_row][c] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_row -= 1
-
-        cur_row = r + 1
-        while cur_row < self.row and self.board[cur_row][c] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_row += 1
-
-        return False
-
-    def check_row(self, r, c, marker):
-        number_of_marker = 0
-
-        cur_col = c
-        while cur_col >= 0 and self.board[r][cur_col] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_col -= 1
-
-        cur_col = c + 1
-        while cur_col < self.col and self.board[r][cur_col] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_col += 1
-
-        return False
-
-    def check_main_diagonal(self, r, c, marker):
-        number_of_marker = 0
-
-        cur_row = r
-        cur_col = c
-        while cur_row >= 0 and cur_col >= 0 and self.board[cur_row][cur_col] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_row -= 1
-            cur_col -= 1
-
-        cur_row = r + 1
-        cur_col = c + 1
-        while cur_row < self.row and cur_col < self.col and self.board[cur_row][cur_col] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_row += 1
-            cur_col += 1
-
-        return False
-
-    def check_secondary_diagonal(self, r, c, marker):
-        number_of_marker = 0
-
-        cur_row = r
-        cur_col = c
-        while cur_row >= 0 and cur_col < self.col and self.board[cur_row][cur_col] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_row -= 1
-            cur_col += 1
-
-        cur_row = r + 1
-        cur_col = c - 1
-        while cur_row < self.row and cur_col >= 0 and self.board[cur_row][cur_col] == marker:
-            number_of_marker += 1
-            if number_of_marker == self.target:
-                return True
-            cur_row += 1
-            cur_col -= 1
-
-        return False
+    def print_winner(self):
+        if self.human_turn:
+            self.human.print_winner()
+        else:
+            self.computer.print_winner()
 
 
 class Player:
-    def __init__(self, marker, board):
+    def __init__(self, marker, bound):
         self.marker = marker
-        self.board = board
+        self.bound = bound
         self.last_row = -1
         self.last_col = -1
-        print("player " + marker + " created")
+        self.validator = InputValidator
+        self.row_col_extractor = RowColExtractor
 
-    def put_marker(self, row, col):
-        print(row, col)
-        if self.board.board[row][col] == ' ':
-            self.board.board[row][col] = self.marker
-            self.last_row = row
-            self.last_col = col
-            return True
-        else:
-            return False
+    def set_last_move(self, row, col):
+        self.last_row = row
+        self.last_col = col
 
-    def decrease_remaining_moves(self):
-        self.board.number_of_remaining_moves -= 1
+    def get_last_row(self):
+        return self.last_row
 
-    def has_won(self):
-        return self.board.is_winning(self.last_row, self.last_col, self.marker)
+    def get_last_col(self):
+        return self.last_col
 
     def print_winner(self):
-        print('Winner is the player with the marker ' + self.marker)
+        print("Player " + self.marker + " has won!")
 
-    def move(self):
+    def make_move(self, board_obj):
         pass
 
 
-class Human_Player(Player):
-    def move(self):
-        run = True
-        while run:
-            try:
-                user_input = input('Enter b/w 1-' + str(self.board.row * self.board.col) + ' : ')
+class InputValidator:
+    @staticmethod
+    def check_validity(input, bound):
+        return 0 < input <= bound
 
-                user_input = int(user_input) - 1
-                if 0 <= user_input < self.board.row * self.board.col:
-                    row = int(user_input / self.board.col)
-                    col = int(user_input % self.board.col)
 
-                    if self.put_marker(row, col):
-                        self.decrease_remaining_moves()
-                        run = False
+class RowColExtractor:
+    @staticmethod
+    def get_row(input, col):
+        return int((input - 1) / col)
+
+    @staticmethod
+    def get_col(input, col):
+        return int((input - 1) % col)
+
+
+class Human(Player):
+
+    def __init__(self, marker, bound):
+        super().__init__(marker, bound)
+        self.io_handler = IOHandler()
+
+    def make_move(self, board_obj):
+
+        while True:
+            user_input = self.io_handler.get_user_input(self.bound)
+            if user_input != -1:
+                if self.validator.check_validity(user_input, self.bound):
+                    cur_row = self.row_col_extractor.get_row(user_input, len(board_obj.board[0]))
+                    cur_col = self.row_col_extractor.get_col(user_input, len(board_obj.board[0]))
+                    if board_obj.check_empty_cell(cur_row, cur_col):
+                        board_obj.put_marker(cur_row, cur_col, self.marker)
+                        self.set_last_move(cur_row, cur_col)
+                        break
                     else:
-                        print('Already entered')
+                        print("Already entered")
                 else:
-                    print('Please enter a valid number')
-            except:
-                print('Enter a number')
-        self.board.print_table()
+                    print("Please enter a valid number")
+            else:
+                print('Please enter a number')
 
 
-class AI_Player(Player):
+class IOHandler:
+    def get_user_input(self, bound):
+        try:
+            user_input = input('Enter b/w 1-' + str(bound) + ' : ')
+            return int(user_input)
+        except:
+            return -1
 
-    def __init__(self, marker, board):
-        super().__init__(marker, board)
-        self.available_slots = list(range(board.row * board.col))
+
+class Computer(Player):
+
+    def __init__(self, marker, bound):
+        super().__init__(marker, bound)
+        self.available_slots = list(range(self.bound))
         self.available_pointer = 0
 
-    def move(self):
+    def make_move(self, board_obj):
 
         index = self.find_slot()
         loc = self.available_slots[index]
 
-        row = int(loc / self.board.col)
-        col = int(loc % self.board.col)
+        cur_row = self.row_col_extractor.get_row(loc, len(board_obj.board[0]))
+        cur_col = self.row_col_extractor.get_col(loc, len(board_obj.board[0]))
+        self.swap(index)
 
-        if self.put_marker(row, col):
-            self.decrease_remaining_moves()
-            self.swap(index)
+        if board_obj.check_empty_cell(cur_row, cur_col):
+            board_obj.put_marker(cur_row, cur_col, self.marker)
+            self.set_last_move(cur_row, cur_col)
         else:
-            self.swap(index)
-            self.move()
-        self.board.print_table()
+            self.make_move(board_obj)
 
     def find_slot(self):
         from random import randrange
@@ -206,36 +142,126 @@ class AI_Player(Player):
         self.available_pointer += 1
 
 
+class Board:
+    def __init__(self, row, col):
+        self.board = []
+        self.initialize_board(row, col)
+        self.number_of_remaining_moves = row * col
+
+    def initialize_board(self, row, col):
+        for r in range(row):
+            self.board.append([])
+            for c in range(col):
+                self.board[r].append(' ')
+
+    def check_empty_cell(self, r, c) -> bool:
+        return self.board[r][c] == ' '
+
+    def put_marker(self, r, c, marker):
+        self.board[r][c] = marker
+
+
+class BoardPrinter:
+    @staticmethod
+    def print(board):
+        row = len(board)
+        col = len(board[0])
+        print('-' * (4 * (col - 1) + 3))
+        for r in range(row):
+            print(' ', end="")
+            for c in range(col):
+                print(str(board[r][c]) + ' ', end="")
+                if c == col - 1:
+                    print(' ')
+                else:
+                    print('| ', end="")
+            print('-' * (4 * (col - 1) + 3))
+
+
+class WinnerChecker:
+    def __init__(self, target: int):
+        self.target = target
+
+    def check(self, board: list, row: int, col: int) -> bool:
+        def count_consecutive_markers(row_inc: int, col_inc: int) -> int:
+            marker = board[row][col]
+            number_of_marker = 0
+            cur_row = row
+            cur_col = col
+
+            while 0 <= cur_row < len(board) and 0 <= cur_col < len(board[0]) and board[cur_row][cur_col] == marker:
+                number_of_marker += 1
+                cur_row += row_inc
+                cur_col += col_inc
+
+            return number_of_marker
+
+        def check_direction(row_dir: int, col_dir: int) -> bool:
+            count = count_consecutive_markers(row_dir, col_dir) + count_consecutive_markers(-row_dir, -col_dir) - 1
+            return count >= self.target
+
+        return check_direction(1, 0) or check_direction(0, 1) or check_direction(1, 1) or check_direction(1, -1)
+
+
+class RemainingMoveCtr:
+    def __init__(self, num_of_remaining_moves: int):
+        self.num_of_remaining_moves = num_of_remaining_moves
+
+    def decrease_num_of_remainig_moves(self):
+        self.num_of_remaining_moves -= 1
+
+    def is_num_of_remaining_moves_zero(self):
+        return self.num_of_remaining_moves == 0
+
+
+class GameEnv:
+    def __init__(self, row, col, target):
+        self.board_obj = Board(row, col)
+        self.printer = BoardPrinter()
+        self.checker = WinnerChecker(target)
+        self.counter = RemainingMoveCtr(row * col)
+        self.player_env = PlayerEnv(row * col)
+
+    def print_board(self):
+        self.printer.print(self.board_obj.board)
+
+    def decrease_num_of_remaining_moves(self):
+        self.counter.decrease_num_of_remainig_moves()
+
+    def check_remaining_moves_is_zero(self):
+        return self.counter.is_num_of_remaining_moves_zero()
+
+    def check_state_is_winning(self):
+        return self.checker.check(self.board_obj.board, self.player_env.get_last_row(), self.player_env.get_last_col())
+
+    def play(self):
+        self.player_env.move(self.board_obj)
+
+    def change_turn(self):
+        self.player_env.change_turn()
+
+    def print_winner(self):
+        self.player_env.print_winner()
+
+
 class TicTacToe:
     def __init__(self, row, col, target):
-        self.board = Board(row, col, target)
-        self.first_player = Human_Player('X', self.board)
-        self.second_player = AI_Player('O', self.board)
+        self.game_env = GameEnv(row, col, target)
+        self.game_env.print_board()
 
     def start(self):
-        self.board.print_table()
 
-        while True:
-
-            if not self.board.is_board_full():
-                self.first_player.move()
-            else:
-                print("Stalemate")
+        while not self.game_env.check_remaining_moves_is_zero():
+            self.game_env.play()
+            self.game_env.print_board()
+            if self.game_env.check_state_is_winning():
+                self.game_env.print_winner()
                 break
+            self.game_env.change_turn()
+            self.game_env.decrease_num_of_remaining_moves()
 
-            if self.first_player.has_won():
-                self.first_player.print_winner()
-                break
-
-            if not self.board.is_board_full():
-                self.second_player.move()
-            else:
-                print("Stalemate")
-                break
-
-            if self.second_player.has_won():
-                self.second_player.print_winner()
-                break
+        if self.game_env.check_remaining_moves_is_zero():
+            print("Stalemate")
 
 
 if __name__ == "__main__":
@@ -258,6 +284,5 @@ if __name__ == "__main__":
                 'Please enter a valid number for target: ')
             target = int(target)
         game = TicTacToe(row, col, target)
-
 
     game.start()
